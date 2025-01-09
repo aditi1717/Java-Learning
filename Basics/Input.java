616d2723866e9b9ff7f223109ee3e4c7714e8863
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-09 20:14:08 +0530

    Update Input.java

--- Basics/Input.java
+++ Basics/Input.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter some input: ");
+
+        //Taking integer type
+//        int rollno=input.nextInt();
+//        System.out.println("Your roll no is "+rollno);
+
+//       Taking string input
+//        String name=input.next();
+//        System.out.println("Your name is "+name);
+        
+//        Taking float input
+        float marks=input.nextFloat();
+        System.out.println("Your marks "+marks);
+
+    }
 
