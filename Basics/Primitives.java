3a1ab7766c5c0af0b743ba3048078ee7cd3a17b5
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-09 19:51:38 +0530

    Update Primitives.java

--- Basics/Primitives.java
+++ Basics/Primitives.java
@@ -1,1 +1,10 @@
-
+public class Primitives {
+    public static void main(String[] args) {
+       int rollno=43;
+       char letter='a';
+       float marks=45.2f;
+       double largeDecimalNumbers=23354564334.455644;
+       long largeIntegerValues=3446364343L;
+       boolean check=true;
+    }
+}
