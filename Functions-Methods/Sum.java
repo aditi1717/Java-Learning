d51ee5fe9f5f97c3c28ab3e31e71d89e97a53181
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-10 22:23:29 +0530

    Update Sum.java

--- Functions-Methods/Sum.java
+++ Functions-Methods/Sum.java
@@ -1,17 +1,35 @@
-
 import java.util.Scanner;
 
-public class Sum{
+public class Main{
     public static void main(String[] args) {
-        sum();
-    }
-    static void sum(){
+            sum();
+            int ans2=sum2();
+            System.out.println(ans2);
+            int ans3=sum3(10,20);
+        System.out.println(ans3);
+        }
+        static void sum(){
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter the first number");
+            int n1=input.nextInt();
+            System.out.println("Enter the second number");
+            int n2=input.nextInt();
+            int sum=n1+n2;
+            System.out.println("Sum of two numbers is "+sum);
+        }
+
+    static int sum2(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the first number");
         int n1=input.nextInt();
         System.out.println("Enter the second number");
         int n2=input.nextInt();
         int sum=n1+n2;
-        System.out.println("Sum of two numbers is "+sum);
+        return sum;
     }
-}
+
+    static int sum3(int n1,int n2){
+        int sum=n1+n2;
+        return sum;
+    }
+    }
