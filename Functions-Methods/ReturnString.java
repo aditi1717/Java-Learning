6d1910843869ae352ee965d8f2cf7004c89f5185
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-10 22:00:01 +0530

    Create ReturnString.java

---
+++ Functions-Methods/ReturnString.java
@@ -0,0 +1,11 @@
+
+public class ReturnString{
+    public static void main(String[] args) {
+        String message =greeting();
+        System.out.println(message);
+    }
+    static String greeting(){
+       String str="How are you";
+        return str;
+    }
+}
