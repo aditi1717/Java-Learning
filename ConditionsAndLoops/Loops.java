5bc6128d9640236568bbcf83b3953f1f785c281e
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-10 13:13:12 +0530

    Create Loops.java

---
+++ ConditionsAndLoops/Loops.java
@@ -0,0 +1,34 @@
+
+public class Main {
+    public static void Loops(String[] args) {
+
+//        print number from 1 to 5 by for loop
+//        for (int num= 1; num<=5 ; num+=2) {
+//            System.out.println(num);
+//    }
+
+
+//            print number from 1 to n
+//            Scanner input=new Scanner(System.in);
+//            int n= input.nextInt();
+//            for (int i = 0; i <=n; i++) {
+//                System.out.print(i);
+//                System.out.println(" Hello world");
+//            }
+
+//        while loop
+//        int num=1;
+//        while(num<=n){
+//            System.out.println("hello"+num);
+//            num++;
+//        }
+
+//        do-while loop
+        int n=1;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n<5);
+
+    }
+}
