439b3f8b47ecfbd077b3659f82f730aff3c8556e
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-14 23:59:04 +0530

    Update FindCeiling.java

--- Binary-Search/FindCeiling.java
+++ Binary-Search/FindCeiling.java
@@ -9,30 +9,22 @@ class FindCeiling{
 
     //find ceiling of a number-smallest number which is larger than or equal to the target 
      static int findCeiling(int[] arr, int target) {
-        boolean isAsc=arr[0]<arr[arr.length-1];
+        if(target > arr[arr.length-1]){
+            return -1;
+        }
         int start=0;
         int end=arr.length-1;
         while (start<=end){
             int mid=start+(end-start)/2;
             if(target==arr[mid]){
                 return mid;
-            }
-            if(isAsc){
-                if(target<arr[mid]){
+            } 
+            else if(target<arr[mid]){
                     end=mid-1;
                 }
-                else{
+            else{
                     start=mid+1;
-                }
-            }
-            else {
-                if(target<arr[mid]){
-                    start=mid+1;
-                }
-                else{
-                    end=mid-1;
-                }
-            }
+                }   
         }
         return start;
     }
