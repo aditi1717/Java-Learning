bb7c8cbb8446eea8f07f72132e98f12d3398ce9b
aditi1717 <144596459+aditi1717@users.noreply.github.com>
2025-01-14 23:25:06 +0530

    Create FindCeiling.java
    find ceiling of a number-smallest number which is larger than the target if we do not find the target

---
+++ Binary-Search/FindCeiling.java
@@ -0,0 +1,40 @@
+
+class FindCeiling{
+    public static void main(String[] args) {
+        int[] arr={12,23,45,47,77};
+        int target=24;
+        int ans=findCeiling(arr,target);
+        System.out.println(ans);
+    }
+
+    //find ceiling of a number-smallest number which is larger than the target if we do not find the target
+     static int findCeiling(int[] arr, int target) {
+        boolean isAsc=arr[0]<arr[arr.length-1];
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(target==arr[mid]){
+                return mid;
+            }
+            if(isAsc){
+                if(target<arr[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
+            }
+            else {
+                if(target<arr[mid]){
+                    start=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
+        }
+        return start;
+    }
+
+}
